87f492ca2e62283e8bdbcd16d1b85643ec902ad5
Jhih Cyuan Shen <shencoop@users.noreply.github.com>
2020-06-15 18:57:35 +0800

    modify

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1gthKIdUJzAuyOP7u_wNDYyvCfhpQsym4XzlvNhFAQfk/';
